106126e243762d28ddae50f8ae39eea7fe2fad58
Yasser Tahiri <yasserth19@gmail.com>
2021-04-27 22:04:20 +0000

    Create 404 page & .htaccess

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,6 +2,7 @@ async function contact() {
     var name = document.querySelector("#name").value;
     var email = document.querySelector("#email").value;
     var message = document.querySelector("#message").value;
+    element.required = true;
     await fetch(
         "https://api.formium.io/submit/6084b5c80304f100014f7bf2/portfolio",
         {
